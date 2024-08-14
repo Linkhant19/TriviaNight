7d527c9a119fee7e6d66517115c3e914d60e41c7
Lin Khant Ko <lkk19@bu.edu>
2024-08-14 16:43:04 -0400

    Initial Commit

---
+++ TriviaNight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriviaNight
+//
+//  Created by Lin Khant Ko on 8/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TriviaNight/TriviaNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaNightApp.swift
+//  TriviaNight
+//
+//  Created by Lin Khant Ko on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
